06da3049381c9865851627e6e9e9cabb060ca180
Abhishek Srivastava <abhishek77in@gmail.com>
2013-09-10 18:55:04 +0530

    Remove redundant files

--- public/JS/RtrRounded.js
+++
@@ -1,8 +0,0 @@
-window.onload=function(){
-if(!NiftyCheck())
-    return;
-RoundedTop("div#container","black","white");
-RoundedBottom("div#container","black","dimgray");
-RoundedTop("div#header","black","#D6D2BC");
-RoundedBottom("div#header","black","#DFDCCB");
-}
